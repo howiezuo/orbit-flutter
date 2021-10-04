5e20904fb0fd774245a19af6d0029ac369b703c8
Howie Zuo <howie.zuo@gmail.com>
2021-10-04 19:03:45 +0900

    icons

--- lib/src/foundation/icons.dart
+++ lib/src/foundation/icons.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/widgets.dart' show IconData;
 
+/// https://orbit.kiwi/foundation/icons/
 class OrbitIcons {
   OrbitIcons._();
 
@@ -7,40 +8,411 @@ class OrbitIcons {
 
   static const account_circle = OrbitIconData(0xe134);
 
+  static const air_conditioning = OrbitIconData(0xe184);
+
   static const airplane = OrbitIconData(0x0061);
+  static const airplane_down = OrbitIconData(0xe101);
+  static const airplane_landing = OrbitIconData(0x0025);
+  static const airplane_return = OrbitIconData(0xe159);
+  static const airplane_takeoff = OrbitIconData(0x002a);
+  static const airplane_up = OrbitIconData(0xe160);
+  static const airplane_up_off = OrbitIconData(0xe102);
+
+  static const airport_security = OrbitIconData(0xe035);
 
   static const alert = OrbitIconData(0xe07f);
-
   static const alert_circle = OrbitIconData(0x0055);
 
+  static const all = OrbitIconData(0xe061);
+
+  static const anywhere = OrbitIconData(0x0067);
+
+  static const apple = OrbitIconData(0xe199);
+
+  static const arrow_down = OrbitIconData(0xe0a0);
+  static const arrow_up = OrbitIconData(0xe09f);
+
+  static const atm = OrbitIconData(0xe02c);
+
+  static const attachment = OrbitIconData(0xe104);
+
+  static const baggage_cabin = OrbitIconData(0xe106);
+  static const baggage_cabin_none = OrbitIconData(0xe174);
+  static const baggage_checked_10 = OrbitIconData(0xe204);
+  static const baggage_checked_20 = OrbitIconData(0xe205);
+  static const baggage_checked_30 = OrbitIconData(0x0068);
+  static const baggage_checked_none = OrbitIconData(0xe021);
+  static const baggage_personal = OrbitIconData(0xe156);
+  static const baggage_personal_none = OrbitIconData(0xe175);
+  static const baggage_recheck = OrbitIconData(0xe105);
+  static const baggage_set = OrbitIconData(0xe107);
+  static const baggage_storage = OrbitIconData(0xe147);
+
+  static const billing_details = OrbitIconData(0xe108);
+
+  static const boarding_gate = OrbitIconData(0xe00a);
+
+  static const boat = OrbitIconData(0xe139);
+
+  static const bug = OrbitIconData(0x004e);
+
+  static const bus = OrbitIconData(0x003c);
+
+  static const cake = OrbitIconData(0xe187);
+
+  static const calendar = OrbitIconData(0x0066);
+  static const calendar_anytime = OrbitIconData(0x0041);
+  static const calendar_range = OrbitIconData(0x007a);
+  static const calendar_trip_length = OrbitIconData(0xe109);
+
+  static const camera = OrbitIconData(0xe017);
+
+  static const car = OrbitIconData(0xe03a);
+  static const car_door = OrbitIconData(0xe185);
+  static const car_rental = OrbitIconData(0xe030);
+
+  static const chart = OrbitIconData(0xe176);
+
+  static const chat = OrbitIconData(0x0039);
+
   static const check = OrbitIconData(0x0056);
+  static const check_circle = OrbitIconData(0x0053);
 
+  static const chevron_double_left = OrbitIconData(0xe202);
+  static const chevron_double_right = OrbitIconData(0xe203);
   static const chevron_down = OrbitIconData(0xe09d);
-
+  static const chevron_left = OrbitIconData(0x0021);
   static const chevron_right = OrbitIconData(0xe01f);
+  static const chevron_up = OrbitIconData(0x006d);
+
+  static const child = OrbitIconData(0xe050);
+  static const child_friendly = OrbitIconData(0xe05d);
 
   static const circle = OrbitIconData(0xe013);
+  static const circle_empty = OrbitIconData(0xe179);
+  static const circle_filled = OrbitIconData(0xe180);
+  static const circle_small = OrbitIconData(0xe148);
+
+  static const city = OrbitIconData(0x0063);
+
+  static const clock = OrbitIconData(0x0065);
 
   static const close = OrbitIconData(0x0058);
-
   static const close_circle = OrbitIconData(0x0074);
+
+  static const cocktail = OrbitIconData(0xe038);
+
+  static const code = OrbitIconData(0xe141);
+
+  static const coffee = OrbitIconData(0xe02f);
+
+  static const color_picker = OrbitIconData(0xe200);
+
+  static const compare = OrbitIconData(0xe155);
+
+  static const compass = OrbitIconData(0xe07e);
+
+  static const contact_email = OrbitIconData(0x0076);
+
+  static const credit_card = OrbitIconData(0x0075);
+
+  static const customer_support = OrbitIconData(0xe194);
+
+  static const deals = OrbitIconData(0xe142);
+
+  static const device_desktop = OrbitIconData(0xe158);
+  static const device_mobile = OrbitIconData(0xe157);
+
+  static const diamond = OrbitIconData(0xe197);
+
+  static const document = OrbitIconData(0xe110);
+
+  static const download = OrbitIconData(0xe014);
+
+  static const duplicate = OrbitIconData(0xe154);
+
+  static const edit = OrbitIconData(0x0045);
+  static const edit_off = OrbitIconData(0xe00d);
+
+  static const email = OrbitIconData(0x0047);
+
+  static const entertainment = OrbitIconData(0xe06e);
+
+  static const exchange = OrbitIconData(0xe032);
+
+  static const facebook = OrbitIconData(0xe002);
+
+  static const feedback = OrbitIconData(0xe140);
+
+  static const filters = OrbitIconData(0x0026);
+
+  static const flight_direct = OrbitIconData(0x0043);
+  static const flight_multicity = OrbitIconData(0x005d);
+  static const flight_nomad = OrbitIconData(0xe150);
+  static const flight_return = OrbitIconData(0x0073);
+  static const flight_services = OrbitIconData(0xe049);
+
+  static const fuel = OrbitIconData(0xe183);
+
+  static const full_screen = OrbitIconData(0xe0a6);
+  static const full_screen_off = OrbitIconData(0xe111);
+
+  static const gallery = OrbitIconData(0xe046);
+
+  static const gender_man = OrbitIconData(0xe152);
+  static const gender_woman = OrbitIconData(0xe151);
+
+  static const google = OrbitIconData(0xe003);
+
+  static const gps_fixed = OrbitIconData(0x0022);
+  static const gps_ios = OrbitIconData(0xe161);
+  static const gps_not_fixed = OrbitIconData(0xe191);
+  static const gps_off = OrbitIconData(0xe01c);
+
+  static const grid = OrbitIconData(0xe06d);
+
+  static const gym = OrbitIconData(0xe089);
+
+  static const history = OrbitIconData(0xe153);
+
+  static const infant = OrbitIconData(0xe186);
 
   static const information_circle = OrbitIconData(0x0052);
 
-  static const lock = OrbitIconData(0xe169);
+  static const instagram = OrbitIconData(0xe112);
 
+  static const insurance = OrbitIconData(0x0027);
+  static const insurance_confirmed = OrbitIconData(0x0072);
+  static const insurance_off = OrbitIconData(0x003a);
+
+  static const invoice = OrbitIconData(0xe113);
+
+  static const item_completed = OrbitIconData(0xe149);
+
+  static const kiwicom = OrbitIconData(0xe143);
+  static const kiwicom_care = OrbitIconData(0xe144);
+  static const kiwicom_guarantee = OrbitIconData(0xe145);
+
+  static const leisure = OrbitIconData(0xe178);
+
+  static const link = OrbitIconData(0xe057);
+
+  static const linkedin = OrbitIconData(0xe114);
+
+  static const list = OrbitIconData(0xe115);
+
+  static const location = OrbitIconData(0x0024);
+  static const location_a = OrbitIconData(0x006a);
+  static const location_add = OrbitIconData(0x003f);
+  static const location_b = OrbitIconData(0x006b);
+  static const location_c = OrbitIconData(0x005e);
+  static const location_d = OrbitIconData(0x005f);
+  static const location_e = OrbitIconData(0x0060);
+  static const location_f = OrbitIconData(0x007b);
+  static const location_g = OrbitIconData(0x007c);
+  static const location_h = OrbitIconData(0x007d);
+  static const location_i = OrbitIconData(0x007e);
+  static const location_j = OrbitIconData(0x005c);
+
+  static const lock = OrbitIconData(0xe169);
   static const lock_open = OrbitIconData(0xe170);
+
+  static const logout = OrbitIconData(0xe0a7);
+
+  static const lounge = OrbitIconData(0xe04e);
+
+  static const map = OrbitIconData(0xe001);
+
+  static const markdown = OrbitIconData(0xe201);
+
+  static const meal = OrbitIconData(0xe116);
+
+  static const menu_hamburger = OrbitIconData(0x0044);
+  static const menu_kebab = OrbitIconData(0xe117);
+  static const menu_meatballs = OrbitIconData(0xe07d);
+
+  static const messages = OrbitIconData(0xe135);
+  static const messages_outline = OrbitIconData(0xe136);
+
+  static const minus = OrbitIconData(0xe118);
+  static const minus_circle = OrbitIconData(0xe119);
+
+  static const money = OrbitIconData(0x0040);
+  static const money_transfer_in = OrbitIconData(0xe167);
+  static const money_transfer_out = OrbitIconData(0xe168);
+
+  static const moon = OrbitIconData(0xe090);
+
+  static const musical_instruments = OrbitIconData(0xe086);
+
+  static const new_window = OrbitIconData(0xe05e);
+
+  static const nonstop = OrbitIconData(0xe120);
+
+  static const notification = OrbitIconData(0xe018);
+  static const notification_add = OrbitIconData(0xe195);
+  static const notification_off = OrbitIconData(0xe023);
+  static const notification_on = OrbitIconData(0xe025);
+
+  static const online_checkin = OrbitIconData(0xe00f);
+  static const online_checkin_off = OrbitIconData(0xe00e);
+
+  static const outlook = OrbitIconData(0xe181);
+
+  static const paid = OrbitIconData(0xe121);
+
+  static const parking = OrbitIconData(0xe03e);
+
+  static const partners = OrbitIconData(0xe164);
+
+  static const passenger = OrbitIconData(0x0077);
+  static const passenger_add = OrbitIconData(0x0029);
+  static const passenger_outline = OrbitIconData(0xe137);
+  static const passenger_remove = OrbitIconData(0xe173);
+  static const passengers = OrbitIconData(0x0028);
+
+  static const passport = OrbitIconData(0x0051);
+
+  static const pet = OrbitIconData(0xe043);
+
+  static const pharmacy = OrbitIconData(0xe034);
+
+  static const phone = OrbitIconData(0xe058);
+
+  static const playground = OrbitIconData(0xe053);
+
+  static const plus = OrbitIconData(0xe122);
+  static const plus_circle = OrbitIconData(0xe027);
+
+  static const pool = OrbitIconData(0xe123);
+
+  static const power_plug = OrbitIconData(0xe06f);
+  static const power_plug_off = OrbitIconData(0x0033);
+
+  static const priority_boarding = OrbitIconData(0xe08d);
+
+  static const profit = OrbitIconData(0xe166);
+
+  static const promo_code = OrbitIconData(0xe081);
+
+  static const qr_code = OrbitIconData(0xe193);
+
+  static const question_circle = OrbitIconData(0x0046);
+
+  static const radar = OrbitIconData(0xe198);
+
+  static const radius_search = OrbitIconData(0x0064);
+
+  static const refund = OrbitIconData(0xe165);
+
+  static const relax = OrbitIconData(0xe046);
+
+  static const reload = OrbitIconData(0x0032);
+
+  static const remove = OrbitIconData(0x0057);
+
+  static const replace = OrbitIconData(0x0049);
+
+  static const restaurant = OrbitIconData(0x0031);
+
+  static const route_no_stops = OrbitIconData(0x006f);
+  static const route_one_stops = OrbitIconData(0x0071);
+  static const route_two_stops = OrbitIconData(0x0070);
 
   static const search = OrbitIconData(0xe07c);
 
+  static const seat = OrbitIconData(0xe02a);
+  static const seat_aisle = OrbitIconData(0xe189);
+  static const seat_extra_legroom = OrbitIconData(0xe190);
+  static const seat_window = OrbitIconData(0xe188);
+
+  static const security = OrbitIconData(0xe146);
+
+  static const self_transfer = OrbitIconData(0xe020);
+
+  static const send = OrbitIconData(0x004d);
+
+  static const settings = OrbitIconData(0x0038);
+
+  static const share = OrbitIconData(0xe016);
+  static const share_android = OrbitIconData(0xe068);
+  static const share_ios = OrbitIconData(0xe163);
+
+  static const shopping = OrbitIconData(0xe036);
+
+  static const show_less = OrbitIconData(0xe124);
+  static const show_more = OrbitIconData(0xe125);
+
+  static const sightseeing = OrbitIconData(0xe015);
+
+  static const smoking = OrbitIconData(0xe137);
+  static const smoking_off = OrbitIconData(0xe126);
+
+  static const sort = OrbitIconData(0xe171);
+
+  static const spa = OrbitIconData(0xe04a);
+
+  static const sport_equipment = OrbitIconData(0xe127);
+  static const sports = OrbitIconData(0xe042);
+
+  static const star_empty = OrbitIconData(0xe08c);
+  static const star_full = OrbitIconData(0x0023);
+
+  static const subway = OrbitIconData(0xe06a);
+
+  static const suitcase = OrbitIconData(0xe02d);
+
+  static const sun = OrbitIconData(0xe096);
+  static const sunrise = OrbitIconData(0xe08e);
+
+  static const taxi = OrbitIconData(0x003b);
+
+  static const terminal = OrbitIconData(0xe196);
+
+  static const terms_and_conditions = OrbitIconData(0xe0a4);
+
+  static const thumb_down = OrbitIconData(0xe128);
+  static const thumb_up = OrbitIconData(0xe01b);
+
   static const ticket = OrbitIconData(0x002e);
+  static const ticket_outline = OrbitIconData(0xe138);
+
+  static const timer = OrbitIconData(0xe177);
+
+  static const tips = OrbitIconData(0xe047);
+
+  static const toilets = OrbitIconData(0xe129);
+
+  static const train = OrbitIconData(0xe028);
+
+  static const trasmission = OrbitIconData(0xe182);
+
+  static const trip = OrbitIconData(0xe130);
+
+  static const twitter = OrbitIconData(0xe131);
+
+  static const uber = OrbitIconData(0xe162);
+
+  static const upload = OrbitIconData(0xe172);
+
+  static const user_group = OrbitIconData(0xe192);
+
+  static const visa = OrbitIconData(0xe132);
+
+  static const visibility = OrbitIconData(0xe005);
+  static const visibility_off = OrbitIconData(0xe004);
+
+  static const walk = OrbitIconData(0xe052);
+
+  static const wallet = OrbitIconData(0xe133);
+
+  static const wheelchair = OrbitIconData(0xe088);
+
+  static const wifi = OrbitIconData(0xe062);
+  static const wifi_off = OrbitIconData(0xe05a);
 }
 
 class OrbitIconData extends IconData {
   const OrbitIconData(int codePoint)
-      : super(
-          codePoint,
-          fontFamily: 'orbit-icons',
-          fontPackage: 'orbit',
-        );
+      : super(codePoint, fontFamily: 'orbit-icons', fontPackage: 'orbit');
 }
