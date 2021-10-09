cef7111cf3eaf30f111f3cd0cb86f8a4be7a83cb
Howie Zuo <howie.zuo@gmail.com>
2021-10-09 18:36:50 +0900

    ListChoice

--- lib/src/components/list_choice.dart
+++ lib/src/components/list_choice.dart
@@ -1,9 +1,8 @@
-import 'package:flutter/widgets.dart';
-import 'package:orbit/orbit.dart';
-import 'package:orbit/src/components/heading.dart';
-import 'package:orbit/src/tokens/text_tokens.dart';
+import 'package:flutter/material.dart';
 
 import '../theme.dart';
+import 'checkbox.dart';
+import 'heading.dart';
 
 class ListChoice extends StatelessWidget {
   final String title;
@@ -34,74 +33,83 @@ class ListChoice extends StatelessWidget {
     final iconStyles = IconTokens.fromDefault(context);
 
     return InkWell(
-      onTap: () => onClick?.call(!isSelected),
-      child: Stack(alignment: AlignmentDirectional.bottomEnd, children: [
-        Container(
-          color: colors.whiteNormal,
-          padding: EdgeInsets.symmetric(
-            vertical: baseTokens.spaceSmall,
-            horizontal: baseTokens.spaceMedium,
-          ),
-          child: Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Row(
+      onTap: () => disabled ? null : onClick?.call(!isSelected),
+      splashColor: disabled ? Palette.Transparent : null,
+      highlightColor: disabled ? Palette.Transparent : null,
+      child: Ink(
+        color: colors.whiteNormal,
+        child: Stack(
+          alignment: AlignmentDirectional.bottomEnd,
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: baseTokens.spaceSmall,
+                horizontal: baseTokens.spaceMedium,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        if (icon != null)
-                          Padding(
-                            padding: EdgeInsets.only(right: baseTokens.spaceXsmall),
-                            child: Icon(
-                              icon,
+                        Row(
+                          children: [
+                            if (icon != null)
+                              Padding(
+                                padding: EdgeInsets.only(
+                                    right: baseTokens.spaceXsmall),
+                                child: Icon(
+                                  icon,
+                                  color: disabled
+                                      ? colors.cloudDarker
+                                      : iconStyles.colorPrimary,
+                                  size: iconStyles.sizeSmall,
+                                ),
+                              ),
+                            Heading.title4(
+                              title,
                               color: disabled
-                                  ? colors.cloudDarker
-                                  : iconStyles.colorPrimary,
-                              size: iconStyles.sizeSmall,
+                                  ? colors.cloudDarkerHover
+                                  : textStyles.colorPrimary,
+                            ),
+                          ],
+                        ),
+                        if (description != null)
+                          Text(
+                            description!,
+                            style: TextStyle(
+                              color: disabled
+                                  ? colors.cloudDarkerHover
+                                  : textStyles.colorSecondary,
+                              fontSize: baseTokens.fontSizeSmall,
+                              fontWeight: baseTokens.fontWeightNormal,
+                              height: textStyles.lineHeightNormal! /
+                                  baseTokens.fontSizeSmall,
                             ),
                           ),
-                        Heading.title4(
-                          title,
-                          color: disabled
-                              ? colors.cloudDarkerHover
-                              : textStyles.colorPrimary,
-                        ),
                       ],
                     ),
-                    if (description != null)
-                      Text(
-                        description!,
-                        style: TextStyle(
-                          color: disabled
-                              ? colors.cloudDarkerHover
-                              : textStyles.colorSecondary,
-                          fontSize: baseTokens.fontSizeSmall,
-                          fontWeight: baseTokens.fontWeightNormal,
-                          height: textStyles.lineHeightNormal! /
-                              baseTokens.fontSizeSmall,
-                        ),
-                      ),
-                  ],
-                ),
+                  ),
+                  SizedBox(width: baseTokens.spaceSmall),
+                  CheckBox(
+                    value: isSelected,
+                    onChanged: onClick,
+                    disabled: disabled,
+                    hasError: hasError,
+                  ),
+                ],
               ),
-              CheckBox(
-                value: isSelected,
-                onChanged: onClick,
-                disabled: disabled,
-                hasError: hasError,
+            ),
+            Positioned(
+              child: Divider(
+                color: colors.cloudNormal,
+                indent: baseTokens.spaceMedium,
+                height: 1,
               ),
-            ],
-          ),
+            ),
+          ],
         ),
-        Positioned(
-          child: Divider(
-            color: colors.cloudNormal,
-            indent: baseTokens.spaceMedium,
-            height: 1,
-          ),
-        ),
-      ]),
+      ),
     );
   }
 }
